a9b19d9b5b9d232f6252d9706951b07c01b84210
Osward José <ojpr15@gmail.com>
2018-02-20 19:42:55 -0430

    'on'

---
+++ preguntas_frecuentes.php
@@ -0,0 +1,19 @@
+<?php
+include('head.php');
+?>
+
+<!-- Page Content -->
+<div class="col-md-12"> 
+  <div class="container">
+    <h1 class="my-4" style="text-align: center;color: #5d78b9">Preguntas Frecuentes
+      <small></small>
+    </h1>
+    <div id="contenedor2">
+      
+    </div><br>
+
+  </div>
+</div>
+<?php
+include('footer.php');
+?>
